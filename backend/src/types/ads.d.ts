b65d748cd42d5b7a98c2b135f26823fb3647134a
Claire <claireyuan.song@gmail.com>
2025-01-16 16:19:19 +0100

    init files

---
+++ backend/src/types/ads.d.ts
@@ -0,0 +1,10 @@
+export type Ad = {
+    [key: string]: string | number;
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    picture: string;
+    location: string;
+};
+
