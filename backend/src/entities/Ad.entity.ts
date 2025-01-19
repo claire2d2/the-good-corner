182167bb67f13fddb44777172afc3efa8d0ce9bf
Claire <claireyuan.song@gmail.com>
2025-01-19 16:07:15 +0100

    adding relations between ad and category

--- backend/src/entities/Ad.entity.ts
+++ backend/src/entities/Ad.entity.ts
@@ -1,23 +1,41 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {
+	Entity,
+	Column,
+	PrimaryGeneratedColumn,
+	CreateDateColumn,
+	UpdateDateColumn,
+    OneToMany,
+    ManyToOne
+} from "typeorm";
+import CategoryEntity from "./Category.entity";
+import { Category } from "../types/category";
 
-@Entity({name: "ads"})
+@Entity({ name: "ads" })
 export default class AdEntity {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
 
-    @Column()
-    title: string;
+	@Column()
+	title: string;
 
-    @Column()
-    description: string;
+	@Column()
+	description: string;
 
-    @Column()
-    price: number;
+	@Column({ type: "float" }) // so that TypeORM does not interpret number as an integer
+	price: number;
 
-    @Column()
-    picture: string;
-    
-    @Column()
-    location: string;
+	@Column()
+	picture: string;
 
+	@Column()
+	location: string;
+
+    @ManyToOne(() => CategoryEntity,(category: Category) => category.ads )
+    category: CategoryEntity;
+
+	@CreateDateColumn()
+	created_at: Date;
+
+	@UpdateDateColumn()
+	updated_at: Date;
 }
---
+++ backend/src/entities/Category.entity.ts
@@ -0,0 +1,27 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	CreateDateColumn,
+	UpdateDateColumn,
+    OneToMany
+} from "typeorm";
+import AdEntity from "./Ad.entity";
+
+@Entity()
+export default class CategoryEntity {
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
+
+	@Column({ unique: true })
+	title: string;
+
+    @OneToMany(() => AdEntity, (ad) => ad.category)
+    ads: AdEntity[]
+
+	@CreateDateColumn()
+	created_at: Date;
+
+	@UpdateDateColumn()
+	updated_at: Date;
+}
