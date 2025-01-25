938b0fc07f2382f7d45792a693ee35674580c24b
Claire <claireyuan.song@gmail.com>
2025-01-25 21:08:59 +0100

    add details

--- frontend/src/pages/OneAd/index.tsx
+++ frontend/src/pages/OneAd/index.tsx
@@ -1,34 +1,47 @@
-import { Ad } from "../../types/Ad"
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import instance from "../../lib/instance"
+import { Ad } from "../../types/Ad";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import instance from "../../lib/instance";
 
 const OneAd = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const [adData, setAdData] = useState<Ad|null>(null)
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [adData, setAdData] = useState<Ad | null>(null);
 
-    const {id} = useParams()
+	const { id } = useParams();
 
-    const getAdData = async() =>{
-        const {data} = await instance.get<{result: Ad}>(`/ads/find/${id}`)
-        setAdData(data.result)
-        setIsLoading(false)
+	const getAdData = async () => {
+    try {
+      const { data } = await instance.get<{ result: Ad }>(`/ads/find/${id}`);
+		setAdData(data.result);
+
+    } catch (error: unknown) {
+      console.log(error)
     }
+    setIsLoading(false);
+	};
 
-    useEffect(() => {
-        getAdData()
-        console.log(adData)
-    },[id])
+	useEffect(() => {
+		getAdData();
+		console.log(adData);
+	}, [id]);
 
-    if (!adData) {
-        return <div>No ad found</div>
-    }
+  if (isLoading) {
+    return <div>Loading ...</div>
+  }
+	if (!adData) {
+		return <div>No ad found</div>;
+	}
 
-  return (
-    <div>
-     Title: {adData?.title}
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1> Title: {adData?.title}</h1>
+      <div>
+        <div>Category : {adData?.category.title}</div>
+        <div>{adData?.location}</div>
+        <div>{adData?.price} </div>
+      </div>
+		</div>
+	);
+};
 
-export default OneAd
+export default OneAd;
