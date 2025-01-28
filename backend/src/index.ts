c22075600f839e48df8a5224d6f802d3a8b68033
Claire <claireyuan.song@gmail.com>
2025-01-28 22:27:42 +0100

    fix image

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,26 @@
 import "reflect-metadata";
+import 'module-alias/register'
+
 
 import express from "express";
-import adsRouter from "./routes/ads.routes"
-import categoriesRouter from "./routes/categories.routes"
-import tagsRouter from "./routes/tags.routes"
+import adsRouter from "./routes/ads.routes";
+import categoriesRouter from "./routes/categories.routes";
+import tagsRouter from "./routes/tags.routes";
 import datasource from "./lib/datasource";
-import cors from "cors"
-
+import cors from "cors";
+import path from "path";
 
 const app = express();
 
-app.use(cors({origin: ["http://localhost:5173"]}))
+app.use(cors({ origin: ["http://localhost:5173"] })); // on autorise le front à communiquer avec notre back
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads"))); // on expose à l'extérieur notre dossier uploads (http://localhost:4000/uploads)
 
 app.use("/ads", adsRouter);
-app.use("/categories", categoriesRouter)
-app.use("/tags", tagsRouter)
+app.use("/categories", categoriesRouter);
+app.use("/tags", tagsRouter);
 
 app.listen(4000, async () => {
-    await datasource.initialize();
-    console.log("Server is launched on port 4000")
-})
-
+  await datasource.initialize(); //initialisation de la base de données
+  console.log("Le serveur est lancé sur le port 4000");
+});
--- frontend/src/pages/OneAd/index.tsx
+++ frontend/src/pages/OneAd/index.tsx
@@ -1,18 +1,21 @@
 import { Ad } from "../../types/Ad";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import instance from "../../lib/instance";
+import { findAd } from "../../lib/requests/ads.requests";
+import defaultPic from "../../assets/picture.png"
 
 const OneAd = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [adData, setAdData] = useState<Ad | null>(null);
 
-	const { id } = useParams();
+	const params = useParams();
 
 	const getAdData = async () => {
 		try {
-			const { data } = await instance.get<{ result: Ad }>(`/ads/find/${id}`);
-			setAdData(data.result);
+			const data = await findAd(params.id!)
+			if (data.success) {
+				setAdData(data.result);
+			}
 		} catch (error: unknown) {
 			console.log(error);
 		}
@@ -21,8 +24,7 @@ const OneAd = () => {
 
 	useEffect(() => {
 		getAdData();
-		console.log(adData);
-	}, [id]);
+	}, []);
 
 	if (isLoading) {
 		return <div>Loading ...</div>;
@@ -38,6 +40,15 @@ const OneAd = () => {
 				<div>Category : {adData?.category.title}</div>
 				<div>{adData?.location}</div>
 				<div>{adData?.price} </div>
+				<img
+        src={
+          adData?.picture
+            ? `${import.meta.env.VITE_BACKEND_URL_FILES}${adData?.picture}`
+            : defaultPic
+        }
+        alt="Image"
+        style={{ maxWidth: "300px", maxHeight: "300px" }}
+      />
 			</div>
 		</div>
 	);
