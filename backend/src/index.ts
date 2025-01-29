8b9f8f186102252e653c8ab2db40fd374c885caa
Claire <claireyuan.song@gmail.com>
2025-01-29 22:18:33 +0100

    init gql files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ import resolvers from "./resolvers";
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({
+const server = new ApolloServer<{}>({
   typeDefs,
   resolvers,
 });
